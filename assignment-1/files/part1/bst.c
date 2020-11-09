8c50c87ff86a3a98d5ffb8ccc7af623b69913c92
Palvinder Sander <23531668+palvindersander@users.noreply.github.com>
2020-11-09 17:54:32 +0000

    Add files via upload

---
+++ assignment-1/files/part1/bst.c
@@ -0,0 +1,169 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+#include "bst.h"
+
+// Your source code here
+
+Node* insertNode(Node *root, int value) {
+
+    // find empty branch
+    // insert new node
+
+    if(root == NULL) {
+        Node *new = (Node*)malloc(sizeof(struct Node));
+        new->data = value;
+        new->left = NULL;
+        new->right = NULL;
+        return new;
+    }
+    else if (value > root->data) {
+        root->right = insertNode(root->right, value);
+    }
+    else {
+        root->left = insertNode(root->left, value);
+    }
+
+    return root;
+
+}
+
+Node* getMinimum(Node *root) {
+
+    if(root == NULL) {
+        return NULL;
+    }
+    else if (root->left != NULL) {
+        return getMinimum(root->left);
+    }
+    else {
+        return root;
+    }
+
+}
+
+Node* deleteNode(Node *root, int value) {
+    
+    // find node to delete
+    // handle no children
+    // handle single child
+    // handle two children
+
+    if(root == NULL) {
+        return NULL;
+    }
+
+    if (value > root->data) {
+        root->right = deleteNode(root->right, value);
+    }
+    else if (value < root->data) {
+        root->left = deleteNode(root->left, value);
+    }
+    else {
+
+        if (root->right == NULL && root->left == NULL) {
+            free(root);
+            return NULL;
+        }
+        else if (root->left == NULL || root->right == NULL) {
+            
+            if(root->left != NULL) {
+                Node *temp = root->left;
+                free(root);
+                return temp;
+            }
+            else {
+                Node *temp = root->right;
+                free(root);
+                return temp;
+            }
+
+        }
+        else {
+            Node *temp = getMinimum(root->right);
+            root->data = temp->data;
+            root->right = deleteNode(root->right, temp->data);
+        }
+
+    }
+
+    return root;
+
+}
+
+void printSubtree(Node *N) {
+
+    // left root right
+
+    if(N != NULL) {
+        printSubtree(N->left);
+        printf("%d\n", N->data);
+        printSubtree(N->right);
+    }
+
+}
+
+int countNodes(Node *N) {
+
+    // root left right
+
+    int count = 0;
+
+    if(N != NULL) {
+        count = count + 1;
+        count = count + countNodes(N->left);
+        count = count + countNodes(N->right);
+    }
+
+    return count;
+}
+
+Node* freeSubtree(Node *N) {
+
+    // left right root
+
+    if(N != NULL) {
+        freeSubtree(N->left);
+        freeSubtree(N->right);
+        free(N);
+    }
+
+    return NULL;
+}
+
+int sumSubtree(Node *N) {
+
+    // root left right
+
+    int count = 0;
+
+    if(N != NULL) {
+        count = count + N->data;
+        count = count + sumSubtree(N->left);
+        count = count + sumSubtree(N->right);
+    }
+
+    return count;
+}
+
+int main()
+{
+	Node *root=NULL;
+	
+	root=insertNode(root, 14);
+	root=insertNode(root, 12);
+	root=insertNode(root, 6);
+	root=insertNode(root, 17);
+	root=insertNode(root, 8);
+	printSubtree(root);
+	printf("\n");
+	printf("%d\n", sumSubtree(root));
+	root=deleteNode(root,14);
+	root=deleteNode(root,8);
+	printSubtree(root);
+	root=freeSubtree(root);
+	printf("Number of nodes=%d\n", countNodes(root));
+
+	return 0;
+}
---
+++ assignment-1/files/part2/serve_client.c
@@ -0,0 +1,30 @@
+pthread_rwlock_t lock = PTHREAD_RWLOCK_INITIALIZER;
+
+
+void* ServeClient(char *client){
+
+	// TODO: Open the client command file and read commands line by line
+
+	// TODO: match and execute commands
+
+	// TODO: Handle command: "insertNode <some unsigned int value>"
+	// print: "[ClientName]insertNode <SomeNumber>\n"
+	// e.g. "[client1_commands]insertNode 1\n"
+
+	// TODO: Handle command: "deleteNode <some unsigned int value>"
+	// print: "[ClientName]deleteNode <SomeNumber>\n"
+	// e.g. "[client1_commands]deleteNode 1\n"
+
+	// TODO: Handle command: "countNodes"
+	// print: "[ClientName]countNodes = <SomeNumber>\n"
+	// e.g. "[client1_commands]countNodes 1\n"
+
+
+	// TODO: Handle command: "sumSubtree"
+	// print: "[ClientName]sumSubtree = <SomeNumber>\n"
+	// e.g. "[client1_commands]sumSubtree 1\n"
+
+
+	return NULL;
+}
+
