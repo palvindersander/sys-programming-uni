a288c8b3158c33532ee1683d8b1d85abbd9c0298
Palvinder Sander <palvinderssander@gmail.com>
2020-11-09 22:33:53 +0000

    added mutex

--- assignment-1/files/part1/bst.c
+++ assignment-1/files/part1/bst.c
@@ -146,24 +146,3 @@ int sumSubtree(Node *N) {
 
     return count;
 }
-
-int main()
-{
-	Node *root=NULL;
-	
-	root=insertNode(root, 14);
-	root=insertNode(root, 12);
-	root=insertNode(root, 6);
-	root=insertNode(root, 17);
-	root=insertNode(root, 8);
-	printSubtree(root);
-	printf("\n");
-	printf("%d\n", sumSubtree(root));
-	root=deleteNode(root,14);
-	root=deleteNode(root,8);
-	printSubtree(root);
-	root=freeSubtree(root);
-	printf("Number of nodes=%d\n", countNodes(root));
-
-	return 0;
-}
--- assignment-1/files/part2/serve_client.c
+++ assignment-1/files/part2/serve_client.c
@@ -1,7 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "bst.h"
+#include "bst.c"
+
 pthread_rwlock_t lock = PTHREAD_RWLOCK_INITIALIZER;
 
 
-void* ServeClient(char *client){
+void* ServeClient(char *clientCommands){
 
 	// TODO: Open the client command file and read commands line by line
 
@@ -24,6 +32,49 @@ void* ServeClient(char *client){
 	// print: "[ClientName]sumSubtree = <SomeNumber>\n"
 	// e.g. "[client1_commands]sumSubtree 1\n"
 
+	FILE *pToFile = fopen(clientCommands,"r");
+	if(pToFile == NULL){
+		return NULL;
+	}
+	char input[512];
+	while(fgets(input,512,pToFile)) {
+		// process line
+		char* line = input;
+		char *saveptr;
+		char *function, *value;
+		function = strtok_r(line, " ", &saveptr);
+		if(function == "insertNode" || function == "deleteNode") {
+			value = strtok_r(NULL, " ", &saveptr);
+			if(value == NULL) {
+				continue;
+			}
+			int val = atoi(value);
+			if(function == "insertNode") {
+				// insert node
+				pthread_rwlock_wrlock(&lock);
+				insertNode(root,val);
+				pthread_rwlock_unlock(&lock);
+				printf("[%d]insertNode %d\n",clientCommands,value);
+			} else if(function == "deleteNode") {
+				// delete node
+				pthread_rwlock_wrlock(&lock);
+				deleteNode(root,val);
+				pthread_rwlock_unlock(&lock);
+				printf("[%d]deleteNode %d\n",clientCommands,value);
+			}
+		} else if(function == "countNodes") {
+			// count nodes
+			pthread_rwlock_rdlock(&lock);
+			countNodes(root);
+			pthread_rwlock_unlock(&lock);
+		} else if(function == "sumSubtree") {
+			// sum nodes
+			pthread_rwlock_rdlock(&lock);
+			sumSubtree(root);
+			pthread_rwlock_unlock(&lock);
+		}
+	}
+	fclose(pToFile);
 
 	return NULL;
 }
